dd884f7d3c92d2f6afd680751d7e557c10cb17a1
Zer02z2 <zongzechan@gmail.com>
2024-03-07 23:38:52 -0500

    done

--- marquee_letter/marquee_letter.ino
+++ marquee_letter/marquee_letter.ino
@@ -1,13 +1,31 @@
-int cycleTime = 1000;
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor   cs = CapacitiveSensor(4,3);
+float progress = 0;
+int lowerThreshold = 10;
+int higherThreshold = 500;
+float cycleTime = higherThreshold;
+
+int LED = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  cs.set_CS_AutocaL_Millis(0xFFFFFFFF); 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  long reading =  cs.capacitiveSensor(30);
+  if (reading > 150) {
+    if (cycleTime > lowerThreshold + 1) cycleTime -= (cycleTime - lowerThreshold) / (higherThreshold - lowerThreshold);
+  }
+  else {
+    if (cycleTime < higherThreshold - 1) cycleTime += (cycleTime - lowerThreshold) / (higherThreshold - lowerThreshold);
+  }
   float degree = PI / cycleTime;
-  int brightness = (sin(degree * millis()) + 1) * 255 / 2;
-  analogWrite(0, brightness);
+  progress += degree;
+  int brightness = (sin(progress) + 1) * 255 / 2;
+  // if (cycleTime <= threshold) analogWrite(LED, 255);
+  analogWrite(LED, brightness);
 }
