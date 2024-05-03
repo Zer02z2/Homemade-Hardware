1eb269fb9f5739e5c752e6a29bff17d2baeef2c4
Zer02z2 <zongzechan@gmail.com>
2024-05-03 04:44:54 -0400

    working!

--- ohmmeter/ohmmeter.ino
+++ ohmmeter/ohmmeter.ino
@@ -1,41 +1,41 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(SCL, SDA, U8X8_PIN_NONE);
 
 #define MAX_ANALOG_VALUE 1023
-#define SWITCH_RESISTANCE 1773.0
+#define SWITCH_RESISTANCE 8
 #define NUM_REF_RESISTORS 8
 #define VCC 3.3
 
 float rRef[NUM_REF_RESISTORS] = { 47, 100, 1000, 10000, 100000, 1000000, 5000000, 10000000 };  // resistors used
 
-const byte rSelPins[3] = { 3, 4, 5 };  // selection pins
-const byte enableMux = 2;              //disable all pins
-const int measurePin = A7;
+const byte rSelPins[3] = { 8, 9, 10 };  // selection pins
+const byte enableMux = 7;               //disable all pins
+const int measurePin = A0;
 
 void setup() {
   // put your setup code here, to run once:
-  analogReadResolution(10);
   pinMode(enableMux, OUTPUT);
   digitalWrite(enableMux, HIGH);
 
-  u8g2.begin();
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_amstrad_cpc_extended_u);
 
   for (int i = 0; i < 3; i++) {  // set each selection pins as OUTPUT
     pinMode(rSelPins[i], OUTPUT);
     digitalWrite(rSelPins[i], HIGH);  // select the highest Rref
   }
 
-  Serial.begin(9600);
-  Serial.println("Starting ohmmeter...");
+  // Serial.begin(9600);
+  // Serial.println("Starting ohmmeter...");
 }
 
 void loop() {
   int cOut;
   float delta, deltaBest1 = MAX_ANALOG_VALUE, deltaBest2 = MAX_ANALOG_VALUE;
   float rBest1 = -1, rBest2 = -1, rR, rX;
-  char unit = 0, fStr[16];
+  char unit = ' ';
   for (byte count = 0; count < NUM_REF_RESISTORS; count++) {
     // Set the Mux select pins to switch in one Rref at a time.
     // count=0: Rref0 (50 ohms), count=1: Rref1 (100 ohms), etc...
@@ -97,59 +97,75 @@ void loop() {
     // Convert the scaled float result to string and extract the units
     //unit = ScaleToMetricUnits(&rX, fStr);
   }
-  ScaleToMetricUnits(&rX, fStr);
 
-  Serial.println(fStr);
-  
-  u8g2.clearBuffer();                    // clear the internal memory
-  u8g2.setFont(u8g2_font_callite24_tr);  // choose a suitable font
-  u8g2.drawStr(0, 40, fStr);   // write something to the internal memory
-  u8g2.sendBuffer();
+  if (rX < 1000) {
+    unit = ' ';
+  } else if (rX >= 1000 && rX < 1000000) {
+    rX /= 1000;
+    unit = 'K';
+  } else if (rX >= 1000000 && rX < 1000000000) {
+    rX /= 1000000;
+    unit = 'M';
+  } else {
+    rX /= 1000000000;
+    unit = 'G';
+  }
+
+  //rX = ScaleToMetricUnits(&rX, fStr);
+
+  //Serial.println(fStr);
+
+  u8x8.setCursor(1, 1);
+  u8x8.print(rX);
+  u8x8.setCursor(1, 4);
+  u8x8.print(unit);
+  u8x8.setCursor(3, 4);
+  u8x8.print("OHM");
+
   //Serial.println(rX);
   //Serial.println("");
   delay(100);
 }
 
-void ScaleToMetricUnits(float *prVal, char fStr[]) {  // calculate the unit, ohm, k, m
-  char unit[1];
+// float ScaleToMetricUnits(float rX, char fStr[]) {  // calculate the unit, ohm, k, m
+//   char unit[1];
 
-  if (*prVal < 1000) {
-    unit[0] = ' ';
-  } else if (*prVal >= 1000 && *prVal < 1000000) {
-    *prVal /= 1000;
-    unit[0] = 'K';
-  } else if (*prVal >= 1000000 && *prVal < 1000000000) {
-    *prVal /= 1000000;
-    unit[0] = 'M';
-  } else {
-    *prVal /= 1000000000;
-    unit[0] = 'G';
-  }
+//   if (rX < 1000) {
+//     unit[0] = ' ';
+//   } else if (rX >= 1000 && rX < 1000000) {
+//     rX /= 1000;
+//     unit[0] = 'K';
+//   } else if (rX >= 1000000 && rX < 1000000000) {
+//     rX /= 1000000;
+//     unit[0] = 'M';
+//   } else {
+//     rX /= 1000000000;
+//     unit[0] = 'G';
+//   }
 
-  for (int k = 2, s = 10; k >= 0; k--, s *= 10) {
-    if ((int)(*prVal) / s == 0) {
-      //dtostrf(*prVal, 4, k, fStr);
-      // Serial.println(snprintf(fStr, 16, "%f", *prVal));
-      String stringVal = "";
+//   fStr[0] = unit[0];
 
-      stringVal += String(int(*prVal)) + "." + String(getDecimal(*prVal));
+//   return rX;
 
-      stringVal.toCharArray(fStr, stringVal.length() + 1);
+//   //sprintf(sz, "%d.%d", val_int, val_fra);
+//   //dtostrf(rX, 4, k, fStr);
+//   // Serial.println(snprintf(fStr, 16, "%f", rX));
+//   // String stringVal = "";
 
-      strcat(fStr, unit);
-      char ohm[4] = "Ohm";
-      strcat(fStr, ohm);
+//   // stringVal += String(int(rX)) + "." + String(getDecimal(rX));
 
-      break;
-    }
-  }
-}
+//   // stringVal.toCharArray(fStr, stringVal.length() + 1);
 
-long getDecimal(float val) {
-  int intPart = int(val);
-  long decPart = 1000 * (val - intPart);          //I am multiplying by 1000 assuming that the foat values will have a maximum of 3 decimal places.
-                                                  //Change to match the number of decimal places you need
-  if (decPart > 0) return (decPart);              //return the decimal part of float number if it is available
-  else if (decPart < 0) return ((-1) * decPart);  //if negative, multiply by -1
-  else if (decPart = 0) return (00);              //return 0 if decimal part of float number is not available
-}
+//   // strcat(fStr, unit);
+//   // char ohm[4] = "Ohm";
+//   // strcat(fStr, ohm);
+// }
+
+// long getDecimal(float val) {
+//   int intPart = int(val);
+//   long decPart = 1000 * (val - intPart);          //I am multiplying by 1000 assuming that the foat values will have a maximum of 3 decimal places.
+//                                                   //Change to match the number of decimal places you need
+//   if (decPart > 0) return (decPart);              //return the decimal part of float number if it is available
+//   else if (decPart < 0) return ((-1) * decPart);  //if negative, multiply by -1
+//   else if (decPart = 0) return (00);              //return 0 if decimal part of float number is not available
+// }
